c25ff30cf3f256b4ec317a9629a30965cbfb3bc1
Jorge Moreira <moreira.ajn@gmail.com>
2023-01-09 18:01:46 +0000

    sort and filtering functions added

--- backend-aspNet/BookCatalogApp/Services/IBookService.cs
+++ backend-aspNet/BookCatalogApp/Services/IBookService.cs
@@ -4,10 +4,12 @@ namespace BookCatalogApp.Services
 {
     public interface IBookService
     {
+
         Task<IEnumerable<Book>> GetBooks();
         Task<Book> GetBook(int id);
         Task<IEnumerable<Book>> GetBooksByISBN(string isbn);
         Task<IEnumerable<Book>> GetBooksByAuthor(string author);
+        Task<IEnumerable<Book>> GetBooksBySearch(string search);
         Task CreateBook(Book book);
         Task UpdateBook(Book book);
         Task DeleteBook(Book book);
